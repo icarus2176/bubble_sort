0008921ae7553774d92f6faf553a96bc5390c628
icarus2176 <ashe.kemuri@gmail.com>
2023-03-06 14:43:47 -0600

    create sorted?method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,13 @@
 def bubble_sort(numbers)
 
 end
+
+def sorted?(numbers)
+  sorted = true
+  numbers.each_with_index do |number, index|
+    if numbers[index] > numbers[index + 1]
+      sorted = false
+  end
+  sorted
+  end
+end
