640390fe9d9209a8ebb3a4552502d9639d46b99f
icarus2176 <ashe.kemuri@gmail.com>
2023-03-06 15:03:33 -0600

    create bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,27 @@
 def bubble_sort(numbers)
-
+  sort_array = numbers
+  until in_order?(sort_array) == true do
+    sort_array.each_with_index do |number, index|
+      if sort_array[index + 1]
+        if number > sort_array[index + 1]
+          sort_array[index], sort_array[index + 1] = sort_array[index + 1], sort_array[index]
+        end
+      end
+    end
+  end
+  sort_array
 end
 
-def sorted?(numbers)
+def in_order?(numbers)
   sorted = true
   numbers.each_with_index do |number, index|
-    if numbers[index] > numbers[index + 1]
-      sorted = false
+    if numbers[index + 1]
+      if number > numbers[index + 1]
+        sorted = false
+    end
+    end
   end
   sorted
-  end
 end
+
+puts bubble_sort([4,3,78,2,0,2])
